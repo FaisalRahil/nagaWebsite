0a24a05d712311952b65db5dce0ca3b75b3d68e9
faroukshouba <faroukshouba@gmail.com>
2015-05-11 09:11:26 +0200

    change web site to php file

---
+++ index_ar.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/nprogress.css" rel="stylesheet">
+    <link href="css/ionicons.css" rel="stylesheet">
+    <link href="js/source/jquery.fancybox.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/fancy-buttons.css" rel="stylesheet">
+    <link href="css/style-contact.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+  </head>
+  <body data-spy="scroll" data-target=".navbar-fixed-top">
+      <div class="navbar navbar-default navbar-fixed-top scroll-me">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a href="#" class="navbar-brand"><img src="img/nagaLogo.png" alt=""/></a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#contact">اتصل بنا</a></li>
+              <li><a href="#team">فريق العمل</a></li>
+              <li><a href="#aboutUs">عن الشركه</a></li>
+              <li><a href="#works">أعمالنا</a></li>
+              <li><a href="#header">الرئيسية</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div id="header">
+        <div class="overlay">
+          <div class="container">
+            <div class="row scroll-me">
+              <div class="col-md-8 col-md-offset-2 col-sm-6 col-sm-offset-3 text-center">
+                <h1 data-scroll-reveal="enter from the bottom after 0.2s">شركة ناقة لتطوير البرمجيات وخلق نظم أفضل ومواقع
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section id="works">
+        <div class="container">
+          <div class="row text-center">
+            <div data-scroll-reveal="enter from the bottom after 0.2s" class="col-md-6 col-md-offset-3 pad-bottom">
+              <h2><strong>WORK PORFOLIO</strong></h2>                    Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae
+            </div>
+          </div>
+          <div class="row text-center">
+            <div data-scroll-reveal="enter from the bottom after 0.4s" class="col-md-12 pad-bottom">
+              <div class="caegories"><a href="#" data-filter="*" class="active btn btn-info btn-sm">All</a><a href="#" data-filter=".html" class="btn btn-info btn-sm">HTML</a><a href="#" data-filter=".css" class="btn btn-info btn-sm">CSS</a><a href="#" data-filter=".code" class="btn btn-info btn-sm">CODE</a><a href="#" data-filter=".script" class="btn btn-info btn-sm">SCRIPT</a></div>
+            </div>
+          </div>
+          <div id="portfolio-div" class="row text-center">
+            <div class="col-md-4 col-sm-4 html">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/1.jpg" class="fancybox-media"><img src="img/portfolio/1.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+            <div class="col-md-4 col-sm-4 css">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/2.jpg" class="fancybox-media"><img src="img/portfolio/2.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+            <div class="col-md-4 col-sm-4 code">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/3.jpg" class="fancybox-media"><img src="img/portfolio/3.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+            <div class="col-md-4 col-sm-4 script">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/4.jpg" class="fancybox-media"><img src="img/portfolio/4.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+            <div class="col-md-4 col-sm-4 html script">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/5.jpg" class="fancybox-media"><img src="img/portfolio/5.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+            <div class="col-md-4 col-sm-4 css code">
+              <div class="work-wrapper"><a title="Image Title Goes Here" href="img/portfolio/6.jpg" class="fancybox-media"><img src="img/portfolio/6.jpg" alt="" class="img-responsive"/></a></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div id="testimonial" data-scroll-reveal="enter from the bottom after 0.2s">
+        <div class="overlay">
+          <div class="container">
+            <div class="row text-center">
+              <div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4"><i class="ion-ios-drag clr-set f-big"></i><br/>
+                <p>
+                  Consectetur adipiscing elit felis dolor felis dolor vitae.
+                  Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae felis dolor vitae
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section id="team">
+        <div class="container">
+          <div data-scroll-reveal="enter from the bottom after 0.2s" class="row text-center">
+            <div class="col-md-6 col-md-offset-3 pad-bottom">
+              <h2><strong>الفريق</strong></h2>                    Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae
+            </div>
+          </div>
+          <div data-scroll-reveal="enter from the bottom after 0.4s" class="row pad-bottom">
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/11.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Technical Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/12.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Ahmed</h3>Ahmed is the founder, CEO, & head of development of Naga. As of December 2012, He has been awarded a master's degree in computer science from the University of San Francisco. In the recent years Ahmed has been employed by the  High National Election Commission in Libya as a tech consultant.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div data-scroll-reveal="enter from the bottom after 0.6s" class="row pad-bottom">
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/13.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/14.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div data-scroll-reveal="enter from the bottom after 0.6s" class="row pad-bottom">
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/17.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/18.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div data-scroll-reveal="enter from the bottom after 0.6s" class="row pad-bottom">
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/19.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+              <div class="media"><a href="#" class="media-left"><img src="img/20.jpg" alt="" class="img-circle"/></a>
+                <div class="media-body">
+                  <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                             3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contact">
+        <div class="overlay">
+          <div data-scroll-reveal="enter from the bottom after 0.4s" class="container contact center">
+            <div class="col-md-3 center section-title">
+              <h2>العنوان 
+                <h4 id="h4">ليبيا طرابلس طريق الصور </h4>
+                <h4 id="h4"><strong>رقم الهاتف  </strong>
+                  <p></p>092******* 
+                  <p></p>091*******
+                </h4>
+                <h4 id="h4"><strong>info@naga.ly : </strong>البريد الالكتروني</h4>
+              </h2>
+              <div class="col-xs-12"><br/><br/></div>
+            </div>
+            <div class="col-md-7 col-md-offset-2">
+              <div class="col-md-10 col-md-offset-1 center section-title">
+                <h2>الرسالة</h2>
+              </div>
+              <div class="confirmation">
+                <p><span class="fa fa-check"></span></p>
+              </div>
+              <form id="contactFormArabic" name="contactFormArabic" methode="POST" class="contact-form support-form">
+                <div class="col-lg-12">
+                  <div id="error"></div>
+                  <input id="contactNameArabic" type="text" name="contactNameArabic" placeholder="الاسم" class="input-field form-item field-name text-right"/>
+                  <input id="contactEmailArabic" type="email" name="contactEmailArabic" placeholder="البريد الالكتروني" class="input-field form-item field-email text-right"/>
+                  <input id="contactSubjectArabic" type="text" name="contactSubjectArabic" placeholder="الموضوع" class="input-field form-item field-subject text-right"/>
+                  <textarea id="contactMessageArabic" rows="7" name="contactMessageArabic" placeholder="الرسالة" class="textarea-field form-item field-message text-right"></textarea>
+                </div>
+                <button type="submit" class="fancy-button button-line button-white large zoom subform">أرسل الرسالة</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>&copy 2015 Naga.ly</footer>
+  </body>
+  <script src="js/jquery-1.11.2.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap-notify.min.js"></script>
+  <script src=""></script>
+  <script src="js/nprogress.js"></script>
+  <script>
+    NProgress.start();
+    $(window).load(function() {
+      NProgress.done(); 
+    });
+  </script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/source/jquery.fancybox.js"></script>
+  <script src="js/jquery.isotope.js"></script>
+  <script src="js/scrollReveal.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="js/jquery.vegas.min.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/custom-contact.js"></script>
+  <script src="js/jquery.validate.min.js"></script>
+  <script src="js/contactValidation.js"></script>
+  <script src="js/contactValidationArabic.js"></script>
+</html>
