0658917edf909b91caf5b09f9cc5637cce889b29
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-05-12 19:53:07 +0200

    data entry

--- index.php
+++ index.php
@@ -48,12 +48,12 @@
       <div class="container">
         <div class="row text-center">
           <div data-scroll-reveal="enter from the bottom after 0.2s" class="col-md-6 col-md-offset-3 pad-bottom">
-            <h2><strong>WORK PORFOLIO</strong></h2>                    Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae
+            <h2><strong>Vision experience and affiliation</strong></h2>                    Finding technical solutions to suit our customers through providing powerful tools & systems that helps manage their businesses with the most favorable prices.
           </div>
         </div>
         <div class="row text-center">
           <div data-scroll-reveal="enter from the bottom after 0.4s" class="col-md-12 pad-bottom">
-            <div class="caegories"><a href="#" data-filter="*" class="active btn btn-info btn-sm">All</a><a href="#" data-filter=".html" class="btn btn-info btn-sm">HTML</a><a href="#" data-filter=".css" class="btn btn-info btn-sm">CSS</a><a href="#" data-filter=".code" class="btn btn-info btn-sm">CODE</a><a href="#" data-filter=".script" class="btn btn-info btn-sm">SCRIPT</a></div>
+            <div class="caegories"><a href="#" data-filter="*" class="active btn btn-info btn-sm">All</a><a href="#" data-filter=".html" class="btn btn-info btn-sm">bootstrap</a><a href="#" data-filter=".css" class="btn btn-info btn-sm">node js</a><a href="#" data-filter=".code" class="btn btn-info btn-sm">wordpress</a><a href="#" data-filter=".script" class="btn btn-info btn-sm">ERP System</a></div>
           </div>
         </div>
         <div id="portfolio-div" class="row text-center">
@@ -78,14 +78,35 @@
         </div>
       </div>
     </section>
+     <section id="aboutUs">
+      <div class="container">
+        <div class="row text-center">
+          <div data-scroll-reveal="enter from the bottom after 0.2s" class="col-md-6 col-md-offset-3 pad-bottom">
+            <h2><strong>About us</strong></h2>                  Naga for IT solutions / was founded in 2014 by a group of Libyan graduates from the University of Tripoli for the purpose of providing professional services that are Libyan and foreign institutes worthy to meet their technical needs, and from the 
+          </div>
+        
+
+
+       </section>
     <div id="testimonial" data-scroll-reveal="enter from the bottom after 0.2s">
       <div class="overlay">
         <div class="container">
           <div class="row text-center">
             <div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4"><i class="ion-ios-drag clr-set f-big"></i><br>
               <p>
-                Consectetur adipiscing elit felis dolor felis dolor vitae.
-                Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae felis dolor vitae
+              <h3>  Static Websites design </h3>
+               </br>
+Since many companies rely on static sites to present their activities and contact information and to communicate with their customers via email, thus Naga offers professional designs to show decent static sites and modern appearance with the activities of companies and meet their needs in communicating with their customers, and take into account the quality of design with competitive price.
+          </br>
+         <h3> Dynamic Websites </h3>
+          </br>
+Naga offers to its customers a range of distinctive designs of performance and quality where it studies the nature of the company's work side by side then proceed with the development of preliminary designs, followed by full design stage where its staff based on stronger competent tools and softwares in the design process, to deliver to the customer a fully reliable product.
+            </br>
+          <h3>  Mobile apps </h3>
+    </br>
+Programming and the development of interactive software and professional applications of the various smartphones, including Android applications among the best technical standards and high quality achieved all the objectives of private companies and institutions and government starting with planning, design, programming and development, testing and analysis and then publishing and marketing. 
+            
+
               </p>
             </div>
           </div>
@@ -96,22 +117,21 @@
       <div class="container">
         <div data-scroll-reveal="enter from the bottom after 0.2s" class="row text-center">
           <div class="col-md-6 col-md-offset-3 pad-bottom">
-            <h2><strong>AWESOME TEAM</strong></h2>                    Consectetur adipiscing elit felis dolor felis dolor vitae.Eelit felis dolor vitae
+            <h2><strong>AWESOME TEAM</strong></h2>                    
           </div>
         </div>
         <div data-scroll-reveal="enter from the bottom after 0.4s" class="row pad-bottom">
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/11.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Technical Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading"> Abdullah Elamir </h3>  abdullah is the naga founder, back end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli
               </div>
             </div>
           </div>
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/12.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Ahmed</h3>Ahmed is the founder, CEO, & head of development of Naga. As of December 2012, He has been awarded a master's degree in computer science from the University of San Francisco. In the recent years Ahmed has been employed by the  High National Election Commission in Libya as a tech consultant.
+                <h3 class="media-heading">Ahmed Fituri</h3>Ahmed is the founder, CEO, & head of development of Naga. As of December 2012, He has been awarded a master's degree in computer science from the University of San Francisco. In the recent years Ahmed has been employed by the  High National Election Commission in Libya as a tech consultant.
               </div>
             </div>
           </div>
@@ -120,16 +140,14 @@
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/13.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">Faisal Rahil</h3>       Faisal is the naga founder,front end developer. He has been awarded a Bachelor's degree in computer science from the University of Tripoli                 
               </div>
             </div>
           </div>
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/14.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">AbdulQudos aboras</h3>      AbdulQudos is the naga founder, front end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli                        
               </div>
             </div>
           </div>
@@ -138,34 +156,31 @@
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/17.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">Farouck Shouba</h3>                           
+                  farouk is the naga founder, front end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli
               </div>
             </div>
           </div>
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/18.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">Aladdin Suni</h3>   Aladdin is the naga founder, front end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli                          
               </div>
             </div>
           </div>
         </div>
         <div data-scroll-reveal="enter from the bottom after 0.6s" class="row pad-bottom">
           <div class="col-md-6 col-sm-6">
-            <div class="media"><a href="#" class="media-left"><img src="img/19.jpg" alt="" class="img-circle"></a>
+            <div class="media"><a href="#" class="media-left"><img src="img/44.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Clients Manager</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">Mohammed Al Gemati</h3>   Mohammed is the naga founder, back end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli                           
               </div>
             </div>
           </div>
           <div class="col-md-6 col-sm-6">
             <div class="media"><a href="#" class="media-left"><img src="img/20.jpg" alt="" class="img-circle"></a>
               <div class="media-body">
-                <h3 class="media-heading">Support Director</h3>                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                           3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+                <h3 class="media-heading">abdulladem frag</h3>    abdo is the naga founder, back end developer . He has been awarded a Bachelor's degree in computer science from the University of Tripoli                                 
               </div>
             </div>
           </div>
@@ -179,8 +194,8 @@
             <h2>Address 
               <h4 id="h4">Triq Asour, Tripoli,Libya</h4>
               <h4 id="h4"><strong>Phone Number : </strong>
-                <p></p>092******* 
-                <p></p>091*******
+                <p></p>0945451959
+                <p></p>0911255848
               </h4>
               <h4 id="h4"><strong>Email : </strong>info@naga.ly</h4>
             </h2>
@@ -207,7 +222,7 @@
         </div>
       </div>
     </section>
-    <footer>&copy 2015 Naga.ly</footer>
+    <footer>Copyright reserved 2015 Naga.ly</footer>
   </body>
   <script src="js/jquery-1.11.2.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
